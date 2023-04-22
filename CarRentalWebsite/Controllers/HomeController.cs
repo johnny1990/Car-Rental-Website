dd5b2e4975a54344d30c9e60cf0c959ab9a18d36
Man Ioan <31920272+johnny1990@users.noreply.github.com>
2023-04-22 19:45:32 +0300

    Add Register Info page

--- CarRentalWebsite/Controllers/HomeController.cs
+++ CarRentalWebsite/Controllers/HomeController.cs
@@ -140,8 +140,13 @@ namespace CarRentalWebsite.Controllers
 
         #endregion
 
+        [HttpGet]
+        public IActionResult RegisterInfo()
+        {
+            return View();
+        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
