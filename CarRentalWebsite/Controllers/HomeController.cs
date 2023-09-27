17462be5e4360c86c592c252535e7e9bfcf89264
Man Ioan <31920272+johnny1990@users.noreply.github.com>
2023-09-27 16:12:48 +0300

    Add Privacy page in project

--- CarRentalWebsite/Controllers/HomeController.cs
+++ CarRentalWebsite/Controllers/HomeController.cs
@@ -182,9 +182,16 @@ namespace CarRentalWebsite.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
         public IActionResult About()
         {
             return View();
-        }       
+        }  
+        
+         public IActionResult Privacy()
+        {
+            return View();
+        }
+
     }
 }
